d87da72c25e24dad609bdec4c43dd5eca87c708e
Maaz Bobat <maazbobat3012@gmail.com>
2025-04-16 15:34:34 -0400

    🔧 updated allowed origins

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,9 @@ const uploadRoutes = require("./routes/uploadRoutes");
 const allowedOrigins = [
   "http://localhost:3000",
   "https://smalobmaaz.vercel.app",
+  "https://www.maazbobat.tech",
   "https://maazbobat.tech",
-  "https://smalob.netlify.app",
+  "https://smalob.netlify.app"
 ];
 
 const { authenticate } = require('./middleware/authMiddleware');
