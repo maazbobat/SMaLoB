6d041e58d4fb6a894cec6b0ea9e09631eb04612c
Maaz Bobat <maazbobat3012@gmail.com>
2025-04-16 15:20:54 -0400

    🔧 Fix CORS to allow Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,6 @@ const uploadRoutes = require("./routes/uploadRoutes");
 const allowedOrigins = [
   "http://localhost:3000",
   "https://smalobmaaz.vercel.app",
-  "https://smalob.netlify.app",
 ];
 
 const { authenticate } = require('./middleware/authMiddleware');
